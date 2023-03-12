9cedded15207f32bbdfb0882f38303d74c0e9367
rhit-LEHMANCB <cblehman22@gmail.com>
2023-03-12 14:42:33 -0400

    organization and added chapter 2 javascript

---
+++ JavaScript/Chapter2/practice.js
@@ -0,0 +1,20 @@
+console.log(typeof 'hello') //string
+console.log(typeof 10) //number
+console.log(typeof true) //boolean
+console.log(typeof { ninja: 'turtle' }) // object
+console.log(typeof [ 1, 2, 3 ]) //object
+
+const name = 'Alexa'; // this value won't change
+
+let score = 0; // this value is a variable and can change
+
+let x = 3, y = 4, z = 5; // can declare multiple variables in one line
+
+score = 5; // reassigning ann already declared variable
+
+// valid examples of variable and constant names
+// $name
+// _answer
+// firstName
+// last_name
+// address_line1
---
+++ JavaScript/quiz/main.js
@@ -0,0 +1,5 @@
+alert('Welcome to Quiz Ninja!');
+
+const question = "What is Superman's real name?";
+const answer = prompt(question);
+alert('You answered ${answer}');
